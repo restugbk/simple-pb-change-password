7e60d4bf4172c462d90dd6192c6dfb93b6586842
Restu Fadhilah <restugbk@i-tech.id>
2025-11-20 12:35:42 +0700

    Update Validation PointBlank 2025

--- lib/function.php
+++ lib/function.php
@@ -75,14 +75,51 @@ function getCookies() {
     return $cookies['SESSION'];
 }
 
+function getValidAuth($getCookies)
+{
+    $url = "https://pointblank.id/check/valid";
+    $cookie = "SESSION=".$getCookies.";";
+    
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => [
+            "Cookie: $cookie",
+            "Cache-Control: max-age=0",
+            "Content-Type: application/x-www-form-urlencoded",
+            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.63 Safari/537.36",
+            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Referer: https://www.pointblank.id/login/form",
+            "Accept-Encoding: gzip, deflate, br",
+            "Accept-Language: en-US,en;q=0.9",
+        ],
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+    return json_decode($response, TRUE);
+}
+
 function authLogin($username, $password, $getCookies)
 {
     $url = "https://www.pointblank.id/login/process";
     $cookie = "SESSION=".$getCookies.";";
+    $getValue = getValidAuth($getCookies);
+    
     $data = [
         'loginFail' => '0',
         'userid' => $username,
-        'password' => $password
+        'password' => $password,
+        'tempValue' => $getValue['tempValue'],
     ];
 
     // Initialize cURL
